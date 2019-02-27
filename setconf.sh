4c8a877b8b5881df6531ad5b09db394a50db6ada
iMac:Ryoji Sawada <radiconkid@users.noreply.github.com>
2019-02-27 11:23:33 +0900

    tmux.conf 改良

--- setconf.sh
+++ setconf.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
 dir=$(cd $(dirname $0);pwd)
 cd $dir
-ln -sf $dir/tmux/tmux.conf ~/.tmux.conf
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+MAC_COPY="ettach-to-user-namespace pbcopy"
+MAC_PASTE="ettach-to-user-namespace pbpaste"
+LILUX_COPY="xclip -i -sel clip > /dev/null"
+LINUX_PASTE="xclip -o -sel clip | tmux load-buffer - ; tmux paste-buffer"
+
+cp -f $dir/tmux/tmux.conf /tmp/.tmux.conf
+if [ $machine = Linux ]; then
+    sed -ie "s/\%COPY\%/\"$LINUX_COPY\"/g" /tmp/.tmux.conf
+    sed -ie "s/\%PASTE\%/\"$LINUX_PASTE\"/g" /tmp/.tmux.conf
+elif [ $machine = "Mac" ]; then
+    sed -ie "s/\%COPY\%/\"$MAC_COPY\"/g" /tmp/.tmux.conf
+    sed -ie "s/\%PASTE\%/\"$MAC_PASTE\"/g" /tmp/.tmux.conf
+fi
+cp -f /tmp/.tmux.conf ~/
